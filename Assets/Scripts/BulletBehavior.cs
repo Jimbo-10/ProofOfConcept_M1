8e472e7bd8a5896961d8772a60f5ee5b8b156578
Jim1007 <jimbo.leimapokpam@georgebrown.ca>
2026-03-12 05:54:56 -0400

    bullet behavior fixed

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -2,30 +2,36 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
-    public float speed = 15f;
+    [SerializeField]
+    float speed = 15f;
 
-    private Rigidbody2D rb;
-    private Camera mainCamera;
+    Rigidbody2D rb;
+    Camera mainCamera;
 
+    UIManager uiManager;
+    EnemyStats stats;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         mainCamera = Camera.main;
 
         rb.linearVelocity = transform.right * speed;
+
+        uiManager = FindObjectOfType<UIManager>();
+        stats = FindObjectOfType<EnemyStats>();
     }
 
     void Update()
     {
-        DestroyIfOutOfView();
+        DestroyBulletOutOfView();
     }
 
-    void DestroyIfOutOfView()
+    void DestroyBulletOutOfView()
     {
-        Vector3 viewportPos = mainCamera.WorldToViewportPoint(transform.position);
+        Vector3 viewPos = mainCamera.WorldToViewportPoint(transform.position);
 
-        if (viewportPos.x < 0 || viewportPos.x > 1 ||
-            viewportPos.y < 0 || viewportPos.y > 1)
+        if (viewPos.x < 0 || viewPos.x > 1 ||
+            viewPos.y < 0 || viewPos.y > 1)
         {
             Destroy(gameObject);
         }
@@ -34,9 +40,35 @@ public class BulletBehavior : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
+        {          
+            if(stats != null)
+            {
+                stats.TakeDamage(50);
+            }
+
+            if (stats.currentHealth <= 0 && uiManager != null)
+            {
+                uiManager.AddKill();
+                Destroy(collision.gameObject);
+            }
+
+            Destroy(gameObject);
+        }
+
+        else if (collision.CompareTag("Zombie"))
         {
-            Destroy(collision.gameObject);
-            Destroy(gameObject);            
+            if (stats != null)
+            {
+                stats.TakeDamage(15);
+            }
+
+            if (stats.currentHealth <= 0 && uiManager != null)
+            {
+                uiManager.AddKill();
+                Destroy(collision.gameObject);
+            }
+
+            Destroy(gameObject);
         }
     }
 }
